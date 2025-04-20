722e5346068826883c1daad87a140a60206f1b90
Luisa Laura <luisa.laura.envio@gmail.com>
2025-04-20 12:12:15 -0300

    apuração de erros, limpando o codigo

--- desafio.py
+++ desafio.py
@@ -1,23 +1,23 @@
 #CLASSIFICANDO HEROIS DESAFIO-DIO/LOGICA DE PROGRAMAÇÃO 
 
-nome= input ("Digite o nome do heroi")
-xp= int(input("Digite a quantidade de (XP) do heroi"))
+nome= input ("Digite o nome do heroi: ")
+xp= int(input("Digite a quantidade de (XP :) do heroi: "))
 
 #NIVEL COM BASE NO XP
 
 if xp <= 1000:
     nivel = "Ferro"
-elif 1001 > xp <= 2000:
+elif 1000 != xp <= 2000:
     nivel = "Bronze"
-elif 2001 <= xp <= 5000:
+elif 2000 != xp <= 5000:
     nivel = "Prata"
-elif 5001 <= xp <= 7000:
+elif 5000 != xp <= 7000:
     nivel = "Ouro"
-elif 7001 <= xp <= 8000:
+elif 7000 != xp <= 8000:
     nivel = "Platina"
-elif 8001 <= xp <= 9000:
+elif 8000 != xp <= 9000:
     nivel = "Ascendente"
-elif 9001 <= xp <= 10000:
+elif 9000 != xp <= 10000:
     nivel = "Imortal"
 else:
     nivel = "Radiante"
@@ -27,3 +27,5 @@ print(f"O Herói de nome {nome} está no nível de {nivel}")
 
 print("Parabéns, continue jogando e subindo de nível!")
 print("Aproveite o jogo e divirta-se!") 
+
+# Codigo limpo  
